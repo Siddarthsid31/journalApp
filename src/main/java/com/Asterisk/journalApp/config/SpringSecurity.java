9a751496a86b1229a8387f7889286090aadb06e8
siddarth <siddarth@hmail.com>
2026-06-03 03:08:23 +0530

    fix: permit /journal/public/** endpoints in security config

--- src/main/java/com/Asterisk/journalApp/config/SpringSecurity.java
+++ src/main/java/com/Asterisk/journalApp/config/SpringSecurity.java
@@ -34,7 +34,7 @@ public class SpringSecurity {
         return http
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/", "/index.html").permitAll()
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/journal/public/**").permitAll()
                         .requestMatchers("/*.html", "/**/*.html", "/css/**", "/js/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/user/**").permitAll()
                         .requestMatchers(
